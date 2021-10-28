ec0e18196e96a4501f3feb6ead7d129378ebd733
eric anaya <ericanaya23@gmail.com>
2021-10-28 09:14:48 -0500

    delete specific words

---
+++ delete.py
@@ -0,0 +1,22 @@
+#remove a word from a txt
+#open up the txt
+f = open('E:\PYTHON REPO\Python_practice\preproinsulin_seq.txt','r')
+#what you want to remove
+a = ['ORIGIN', '1', '6', '//']
+#empty listt your gonna input new data in
+lst = []
+#reading the lines in the files
+for line in f:
+    #going through the words
+    for word in a:
+        #if the word exist delete it
+        if word in line:
+            line = line.replace(word,'')
+    lst.append(line)
+f.close()
+#creating new file
+f = open('E:\PYTHON REPO\Python_practice\preproinsulin_seq_clean.txt','w')
+#writing new txt file
+for line in lst:
+    f.write(line)
+f.close()
